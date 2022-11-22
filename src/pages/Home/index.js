4cae61adc844ae5523eb9e23a1cc158d11e46684
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-11-22 20:46:25 -0300

    Criando componente de movimentações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Header from '../../components/Header';
 import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+
+const list = [
+    {
+        id: 1,
+        label: 'Boleto conta luz',
+        value: '300,90',
+        date: '17/09/2022',
+        type: 0 //despesas
+    },
+
+    {
+        id: 2,
+        label: 'Pix Cliente X',
+        value: '2.500,00',
+        date: '22/10/2022',
+        type: 1 //receita
+    },
+
+    {
+        id: 3,
+        label: 'Salário',
+        value: '7.200,00',
+        date: '10/09/2022',
+        type: 1 //receita
+    },
+]
 
 export default function Home() {
     return (
@@ -9,6 +36,16 @@ export default function Home() {
             <Header name='Herbert Carlos' />
 
             <Balance saldo='9.250,90' gastos='-527,00'/>
+
+            <Text style={styles.title}>Ultimas Movimentações</Text>
+
+            <FlatList
+                style={styles.list}
+                data={list}
+                keyExtractor={(item) => String(item.id)}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => <Movements data={item}/>}
+            />
         </View>
     );
 }
@@ -18,4 +55,16 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fafafa',
     },
+
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        margin: 14,
+        
+    },
+
+    list: {
+        marginStart: 14,
+        marginEnd: 14
+    }
 });
